3954087420fdb5e0e2f44d8e72666352a97efb7f
andres.aguilar10@hotmail.com <andres.aguilar10@hotmail.com>
2020-10-28 14:59:32 -0600

    File loading and show diccionario implemented

--- src/Menu.hs
+++ src/Menu.hs
@@ -1,8 +1,9 @@
 module Menu where
 
-import Data.Map.Lazy (fromList)
+import Data.Map ()
+import qualified Data.Map as Map
 import DataTypes (HypMap)
-import System.IO (IO, getLine, putStr, putStrLn)
+import System.IO
 import TextAligning ()
 import Prelude hiding (filter, lookup, map, null)
 
@@ -12,13 +13,13 @@ type Estado = HypMap
 -- el cual recibe el Estado como parámetro
 main :: IO ()
 main = do
-  mainloop (fromList [])
+  mainloop (Map.fromList [])
 
 -- Ciclo de ejecución:
---     1. Recibe un Estado
---     2. lee un comando
---     3. Ejecuta un comando que produce un nuevo Estado
---     4. Se invoca recursivamente con el nuevo Estado.
+--  1. Recibe un Estado
+--  2. lee un comando
+--  3. Ejecuta un comando que produce un nuevo Estado
+--  4. Se invoca recursivamente con el nuevo Estado.
 mainloop :: Estado -> IO ()
 mainloop estado = do
   putStr ">> "
@@ -27,11 +28,45 @@ mainloop estado = do
   let comando = tokens !! 0
   -- Check for command
   case comando of
-    "leer" -> do
-      putStrLn ">>> Hola mundo"
+    "load" -> do
+      -- Get file name
+      let nombreArchivo = (tokens !! 1)
+      -- Create handle
+      inh <- openFile nombreArchivo ReadMode
+      -- New state
+      nuevoEstado <- loadDiccionario inh estado
+      hClose inh
+      putStrLn (("Diccionario cargado ") ++ (show (length (Map.keys nuevoEstado))) ++ " palabras cargadas")
+      mainloop nuevoEstado
+    "show" -> do
+      putStrLn (show estado)
       mainloop estado
-    "fin" -> do
+    "exit" -> do
       putStrLn "Saliendo..."
     _ -> do
       putStrLn $ "Comando desconocido (" ++ comando ++ "): '" ++ inpStr ++ "'"
       mainloop estado
+
+-- Recibe el estado actual, el token y sus silabas
+-- Agrega un token nuevo al diccionario
+addToken :: Estado -> String -> [String] -> Estado
+addToken estado token silabas =
+  -- Check for diccionary member
+  if Map.member token estado
+    then estado
+    else Map.insert token silabas estado
+
+-- Recibe el handle del archivo y el estado actual
+-- Carga el diccionario a partir de un handle del archivo
+loadDiccionario :: Handle -> Estado -> IO Estado
+loadDiccionario inh estado = do
+  ineof <- hIsEOF inh
+  if ineof
+    then return estado
+    else do
+      inpStr <- hGetLine inh
+      let fileLine = words (inpStr)
+      let silabas = fileLine !! 1
+      -- Insert new slot for diccionary
+      let nuevoEstado = addToken estado (head fileLine) (words [if c == ',' then ' ' else c | c <- silabas])
+      loadDiccionario inh nuevoEstado
